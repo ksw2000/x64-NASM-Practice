57b0f4feb26e585a8441385cd38ba190e02aeb68
liao2000 <liao2000@livemail.tw>
2022-01-01 22:12:20 +0800

    Update stdio

--- 01-stdio/print.asm
+++ 01-stdio/print.asm
@@ -1,11 +1,30 @@
-EXTERN printf
+;-----------------------------------------------------------------
+; An example for using clib printf() in NASM
+;
+; Assemble:
+; nasm -f win64 {filename}.asm
+;
+; Link:
+; gcc {filename}.obj -o {filename}.exe
+; goLink /console {filename}.obj /entry main msvcrt.dll
+; 
+; Reference:
+; https://cs.lmu.edu/~ray/notes/nasmtutorial/
+;-----------------------------------------------------------------
+        global main
+        extern printf                   ; clib function you want to call
 
-SECTION .data
-    msg DB 'Hello World 是在哈囉', 0
+        section .data                   ; initialized data
+msg:    db 'Hello World 是在哈囉', 0
+        ; db (define byte)              8 bits
+        ; dw (define word)             16 bits
+        ; dd (define double word)      32 bits
+        ; dq (define quad word)        64 bits
 
-SECTION .text
-start:
-    mov    rcx, msg
-    sub    rsp, 32
-    call   printf
-    add    rsp, 32
+        section .text                   ; main program
+main:
+        mov    rcx, msg
+        sub    rsp, 32
+        call   printf
+        add    rsp, 32
+        ret
--- 01-stdio/scan.asm
+++ 01-stdio/scan.asm
@@ -1,42 +1,44 @@
-EXTERN printf, scanf
+;-------------------------------------------------------
+; Example of using clib scanf() in NASM
+;
+; Assemble:
+; nasm -f win64 {filename}.asm
+;
+; Link:
+; gcc {filename}.obj -o {filename}.exe
+; goLink /console {filename}.obj /entry main msvcrt.dll
+;-------------------------------------------------------
+        global main
+        extern printf
+        extern scanf
+        section .data
+msg:    db   'keyin a dword int:', 0
+msg2:   db   0xd, 0xa, 'output...' , 0
+fmt:    db   '%d', 0
+fmt2:   db   'you input %d', 0
 
-SECTION .data
-    msg    DB   'keyin a dword int:', 0
-    msg2   DB   0xd, 0xa, 'output...' , 0
-    fmt    DB   '%d', 0
-    fmt2   DB   'you input %d', 0
+        section .bss
+in:     resd 1
 
-SECTION .bss
-    in:    RESD 1
+        section .text
+main:
+        ; printf(msg)
+        mov    rcx, msg
+        sub    rsp, 32
+        call   printf
+        add    rsp, 32
 
-SECTION .text
-start:
-    ; 印出 msg
-    push   rcx
-    mov    rcx, msg
-    sub    rsp, 32
-    call   printf
-    add    rsp, 32
-    pop    rcx
+        ; scanf(fmt, &in)
+        mov    rcx, fmt
+        mov    rdx, in
+        sub    rsp, 32
+        call   scanf
+        add    rsp, 32
 
-    ; 輸入
-    push   rcx
-    push   rdx
-    mov    rcx, fmt  ;格式參數位址
-    mov    rdx, in   ;參數輸入位址
-    sub    rsp, 32
-    call   scanf
-    add    rsp, 32
-    pop    rdx
-    pop    rcx
-
-    ; 印出 in
-    push   rcx
-    push   rdx
-    sub    rsp, 32
-    mov    rcx, fmt2
-    mov    rdx, [in]
-    call   printf
-    add    rsp, 32
-    pop    rdx
-    pop    rcx
+        ; printf(fmt2, in)
+        sub    rsp, 32
+        mov    rcx, fmt2
+        mov    rdx, [in]
+        call   printf
+        add    rsp, 32
+        ret
