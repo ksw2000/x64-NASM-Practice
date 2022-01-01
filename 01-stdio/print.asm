2f743b91a7d1fd751d60061697c391da296f75ea
liao2000 <liao2000@livemail.tw>
2022-01-02 01:17:02 +0800

    format

--- 01-stdio/print.asm
+++ 01-stdio/print.asm
@@ -8,14 +8,22 @@
 ; gcc {filename}.obj -o {filename}.exe
 ; goLink /console {filename}.obj /entry main msvcrt.dll
 ; 
+; Run: (command line)
+; chcp 65001
+; {filename}.exe
+;
+; If the output is garbled:
+; https://stackoverflow.com/questions/57131654/using-utf-8-encoding-chcp-65001-in-command-prompt-windows-powershell-window
+; 
+;
 ; Reference:
 ; https://cs.lmu.edu/~ray/notes/nasmtutorial/
 ;-----------------------------------------------------------------
-        global main
-        extern printf                   ; clib function you want to call
+        global  main
+        extern  printf                  ; clib function you want to call
 
         section .data                   ; initialized data
-msg:    db 'Hello World 是在哈囉', 0
+msg:    db      'Hello World 是在哈囉', 0
         ; db (define byte)              8 bits
         ; dw (define word)             16 bits
         ; dd (define double word)      32 bits
--- 01-stdio/scan.asm
+++ 01-stdio/scan.asm
@@ -8,37 +8,38 @@
 ; gcc {filename}.obj -o {filename}.exe
 ; goLink /console {filename}.obj /entry main msvcrt.dll
 ;-------------------------------------------------------
-        global main
-        extern printf
-        extern scanf
+        global  main
+        extern  printf
+        extern  scanf
+
         section .data
-msg:    db   'keyin a dword int:', 0
-msg2:   db   0xd, 0xa, 'output...' , 0
-fmt:    db   '%d', 0
-fmt2:   db   'you input %d', 0
+msg:    db      'keyin a dword int:', 0
+msg2:   db      0xd, 0xa, 'output...' , 0
+fmt:    db      '%d', 0
+fmt2:   db      'you input %d', 0
 
         section .bss
-in:     resd 1
+in:     resd    1
 
         section .text
 main:
         ; printf(msg)
-        mov    rcx, msg
-        sub    rsp, 32
-        call   printf
-        add    rsp, 32
+        mov     rcx, msg
+        sub     rsp, 32
+        call    printf
+        add     rsp, 32
 
         ; scanf(fmt, &in)
-        mov    rcx, fmt
-        mov    rdx, in
-        sub    rsp, 32
-        call   scanf
-        add    rsp, 32
+        mov     rcx, fmt
+        mov     rdx, in
+        sub     rsp, 32
+        call    scanf
+        add     rsp, 32
 
         ; printf(fmt2, in)
-        sub    rsp, 32
-        mov    rcx, fmt2
-        mov    rdx, [in]
-        call   printf
-        add    rsp, 32
+        sub     rsp, 32
+        mov     rcx, fmt2
+        mov     rdx, [in]
+        call    printf
+        add     rsp, 32
         ret
